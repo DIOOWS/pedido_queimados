a10a176333e65a95daacf2266d9ad67274d538b6
DIOOWS <diogoalexsilva123@gmail.com>
2025-12-12 18:16:33 -0300

    Fix cloudinary storage config

--- requisicoes/settings.py
+++ requisicoes/settings.py
@@ -33,12 +33,6 @@ INSTALLED_APPS = [
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
-CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
-    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
-}
-
 # ===============================
 # MIDDLEWARE
 # ===============================
